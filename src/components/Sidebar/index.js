862ddf302ebd66109f407dd02256dc849f68196c
Faye Dumbrigue <fayedumbrigue@gmail.com>
2025-05-03 18:19:15 -0600

    Add social buttons to nav bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,11 +3,18 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faLinkedin,
+  faGithub,
+  faPhoneVolume
+} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
-  <div className='nav-bar'>
-    <Link className='logo' to='/'>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
       <img src={LogoS} alt="logo" />
       <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
     </Link>
@@ -15,13 +22,43 @@ const Sidebar = () => (
       <NavLink exact="true" activeclassname="active" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/dumbrigue/"
+        >
+          <FontAwesomeIcon icon={faPhoneVolume} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/fayesadev"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
