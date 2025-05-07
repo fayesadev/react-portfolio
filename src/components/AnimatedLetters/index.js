019e874473599a2ac2fecc20c1847c6f18db1d75
Faye Dumbrigue <fayedumbrigue@gmail.com>
2025-05-07 15:08:51 -0600

    Add hover animation css for animated letter

--- src/components/AnimatedLetters/index.js
+++ src/components/AnimatedLetters/index.js
@@ -1,15 +1,13 @@
-import './index.scss';
+import './index.scss'
 
 const AnimatedLetters = ({ letterClass, strArray, idx }) => {
   return (
     <span>
-      {
-        strArray.map((char, i) => (
-          <span key={char + i} className={`${letterClass} _${i + idx}`}>
-            {char}
-            </span>
-        ))
-      }
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
     </span>
   )
 }
