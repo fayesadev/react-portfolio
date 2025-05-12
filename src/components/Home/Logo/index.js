7d8f2190431200108de12d6e49db7a55bde08387
Faye Dumbrigue <fayedumbrigue@gmail.com>
2025-05-12 12:04:06 -0600

    Add gsap to animate logo

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,17 +1,44 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import gsap from 'gsap-trial'
 
 const Logo = () => {
+  const bgRef = useRef()
+  const outlineLogoRef = useRef()
+  const solidLogoRef = useRef()
 
-  const bgRef = useRef();
-  const outlineLogoRef = useRef();
-  const solidLogoRef = useRef();
+  useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin)
 
+    gsap
+      .timeline()
+      .to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
+      .from(outlineLogoRef.current, {
+        drawSVG: 0,
+        duration: 20,
+      })
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
+  }, [])
 
   return (
-    <div className='logo-container'>
-      <img className='solid-logo' src={LogoS} alt="S" />
+    <div className="logo-container" ref={bgRef}>
+      <img ref={solidLogoRef} className="solid-logo" src={LogoS} alt="S" />
       <svg
         width="559pt"
         height="897pt"
