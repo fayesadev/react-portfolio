c1404e217d637fe962c2f5b28f019323283f2805
Faye Dumbrigue <fayedumbrigue@gmail.com>
2025-05-22 10:59:25 -0600

    Init Contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,47 @@
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import Loader from 'react-loaders'
+import { useEffect, useState } from 'react'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={'Contact Me'.split('')}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+          </p>
+          <div className='contact-form'>
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
