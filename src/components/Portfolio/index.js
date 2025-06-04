2c65a4b0e7bb15c9b6eb62fda551b0dfd106113c
Faye Dumbrigue <fayedumbrigue@gmail.com>
2025-06-04 08:52:21 -0600

    Style portfolio content

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,6 +26,13 @@ const Portfolio = () => {
                 className="portfolio-image"
                 alt="portfolio"
               />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
             </div>
           )
         })}
@@ -35,16 +42,14 @@ const Portfolio = () => {
   return (
     <>
       <div className="container portfolio-page">
-        
-          <h1 className="page-title">
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={portfolioArray}
-              idx={15}
-            />
-          </h1>
-          <div>{renderPortfolio(portfolioData.portfolio)}</div>
-       
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={portfolioArray}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
       <Loader type="line-scale-pulse-out" />
     </>
